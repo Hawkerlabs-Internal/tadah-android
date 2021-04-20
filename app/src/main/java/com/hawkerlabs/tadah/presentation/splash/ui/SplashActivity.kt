4ed29c84f396e6ca303f19386fca8987edaf2cdd
hawkerlabs <hawkerlabs@gmail.com>
2021-04-20 11:40:42 -0700

    some changes

--- app/src/main/java/com/hawkerlabs/tadah/presentation/splash/ui/SplashActivity.kt
+++ app/src/main/java/com/hawkerlabs/tadah/presentation/splash/ui/SplashActivity.kt
@@ -6,6 +6,7 @@ import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.hawkerlabs.tadah.R
 import com.hawkerlabs.tadah.databinding.ActivitySplashBinding
 import com.hawkerlabs.tadah.presentation.MainActivity
@@ -19,10 +20,18 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
 
+        Glide.with(this)
+                .asBitmap()
+                .load(SPLASH_IMAGE_URL)
+                .centerInside()
+                .into(binding.splashImg)
+
         Handler(Looper.getMainLooper()).postDelayed({ startActivity(Intent(this, MainActivity::class.java)) }, SPLASH_SCREEN_DELAY)
     }
 
     companion object {
         private const val SPLASH_SCREEN_DELAY = 3000L
+        private const val SPLASH_IMAGE_URL = "https://tadah-hlabs.s3.ap-south-1.amazonaws.com/splash.png"
+
     }
 }
